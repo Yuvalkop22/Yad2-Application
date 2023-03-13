9bdfa7d4d8eb102775792ce9fde59671945dfbdb
Yuvalkop22 <Kopelman050@gmail.com>
2023-03-13 21:13:18 +0200

    Initial commit

---
+++ app/src/main/java/com/example/yad2application/SignUpFragment.java
@@ -0,0 +1,112 @@
+package com.example.yad2application;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.core.view.MenuProvider;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.Lifecycle;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.yad2application.Model.Model;
+import com.example.yad2application.Model.Student;
+import com.example.yad2application.databinding.FragmentSignUpBinding;
+
+public class SignUpFragment extends Fragment {
+    FragmentSignUpBinding binding;
+    ActivityResultLauncher<Void> cameraLauncher;
+    ActivityResultLauncher<String> galleryLauncher;
+    Boolean isAvatarSelected = false;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FragmentActivity parentActivity = getActivity();
+        parentActivity.addMenuProvider(new MenuProvider() {
+            @Override
+            public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+                menu.removeItem(R.id.signUpFragment);
+            }
+
+            @Override
+            public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+                return false;
+            }
+        },this, Lifecycle.State.RESUMED);
+
+        cameraLauncher = registerForActivityResult(new ActivityResultContracts.TakePicturePreview(), new ActivityResultCallback<Bitmap>() {
+            @Override
+            public void onActivityResult(Bitmap result) {
+                if (result != null) {
+                    binding.avatarImg.setImageBitmap(result);
+                    isAvatarSelected = true;
+                }
+            }
+        });
+        galleryLauncher = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+            @Override
+            public void onActivityResult(Uri result) {
+                if (result != null){
+                    binding.avatarImg.setImageURI(result);
+                    isAvatarSelected = true;
+                }
+            }
+        });
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentSignUpBinding.inflate(inflater,container,false);
+        View view = binding.getRoot();
+
+        binding.btnSignUp.setOnClickListener(view1 -> {
+            String name = binding.username.getText().toString();
+            String stId = binding.password.getText().toString();
+            Student st = new Student(stId,name,"",false);
+
+            if (isAvatarSelected){
+                binding.avatarImg.setDrawingCacheEnabled(true);
+                binding.avatarImg.buildDrawingCache();
+                Bitmap bitmap = ((BitmapDrawable) binding.avatarImg.getDrawable()).getBitmap();
+                Model.instance().uploadImage(stId, bitmap, url->{
+                    if (url != null){
+                        st.setAvatarUrl(url);
+                    }
+                    Model.instance().addStudent(st, (unused) -> {
+                        Navigation.findNavController(view1).popBackStack();
+                    });
+                });
+            }else {
+                Model.instance().addStudent(st, (unused) -> {
+                    Navigation.findNavController(view1).popBackStack();
+                });
+            }
+        });
+
+        //binding.cancellBtn.setOnClickListener(view1 -> Navigation.findNavController(view1).popBackStack(R.id.studentsListFragment,false));
+
+        binding.cameraButton.setOnClickListener(view1->{
+            cameraLauncher.launch(null);
+        });
+
+        binding.galleryButton.setOnClickListener(view1->{
+            galleryLauncher.launch("image/*");
+        });
+        return view;
+    }
+}
